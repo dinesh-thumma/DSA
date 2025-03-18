66b39a536220f1226550282787596375473c75b0
Dinesh Thumma <dineshthumma.2404@gmail.com>
2025-03-18 22:41:26 +0530

    solved one more problem

--- C++/2. Arrays/array_sorted_rotated.cpp
+++ C++/2. Arrays/array_sorted_rotated.cpp
@@ -1,14 +1,22 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
+using namespace std;
+int main() {
+    ifstream inputFile("input.txt");
+    string input;
+    inputFile >> input;
+    cout << input;
+    return 0;
+}
 class Solution {
     public:
-        void rotateArray(vector<int>& arr) {
+        void rotateArray(vector <int>& arr) {
             int temp = arr[0];
             for (int i = 0; i < arr.size() - 1; i++) {
                 arr[i] = arr[i + 1];
             }
             arr[arr.size() - 1] = temp;
         }
-        bool areTheySame(vector<int>& arr1, vector<int>& arr2) {
+        bool areTheySame(vector <int>& arr1, vector <int>& arr2) {
             for (int i = 0; i < arr1.size(); i++) {
                 if (arr1[i] != arr2[i]) {
                     return false;
@@ -16,8 +24,8 @@ class Solution {
             }
             return true;
         }
-        bool check(vector<int>& nums) {
-            vector<int> arr(nums);
+        bool check(vector <int>& nums) {
+            vector <int> arr(nums);
             int n = nums.size();
             sort(arr.begin(), arr.end());
             for (int i = 0; i < n; i++) {
@@ -26,6 +34,7 @@ class Solution {
                     return true;
                 }
             }
-            return false;
-        }
-    };
+        return false;
+    }
+    
+};
